83a0eea6c83e0d93ed92d8aa49f376bace21e358
jaehosung <jaehossung@gmail.com>
2018-06-03 23:57:05 +0900

    ENH: Bullet Generating

--- game/Assets/PlayerMovement.cs
+++ game/Assets/PlayerMovement.cs
@@ -7,47 +7,47 @@ public class PlayerMovement : MonoBehaviour
 {
 
 
-	public float speed = 6f;
+	public float Speed = 6f;
 
-	private Vector3 movement;
-	private Rigidbody playerRigidbody;
-	private int floorMask;
-	private float camRayLength = 1000f;
+	private Vector3 _movement;
+	private Rigidbody _playerRigidbody;
+	private int _floorMask;
+	private readonly float camRayLength = 1000f;
 
 	void Awake()
 	{
-		floorMask = LayerMask.GetMask("Floor");
-		playerRigidbody = GetComponent<Rigidbody>();
+		_floorMask = LayerMask.GetMask("Floor");
+		_playerRigidbody = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate ()
 	{
-		float h = Input.GetAxisRaw("Horizontal");
-		float v = Input.GetAxisRaw("Vertical");
+		var h = Input.GetAxisRaw("Horizontal");
+		var v = Input.GetAxisRaw("Vertical");
 		Move(h,v);
 		Turning();
 	}
 
 	void Move(float h, float v)
 	{
-		movement.Set(h, 0f, v);
-		movement = movement.normalized * speed * Time.deltaTime;
-		playerRigidbody.MovePosition(transform.position+movement);
+		_movement.Set(h, 0f, v);
+		_movement = _movement.normalized * Speed * Time.deltaTime;
+		_playerRigidbody.MovePosition(transform.position+_movement);
 	}
 
 	void Turning()
 	{
-		Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+		var camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
 		RaycastHit floorHit;
 
-		if (Physics.Raycast(camRay, out floorHit, camRayLength, floorMask))
+		if (Physics.Raycast(camRay, out floorHit, camRayLength, _floorMask))
 		{
 			Vector3 playerToMouse = floorHit.point - transform.position;
 			playerToMouse.y = 0f;
 
 			Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
-			playerRigidbody.MoveRotation(newRotation);
+			_playerRigidbody.MoveRotation(newRotation);
 		}
 	}
 }
