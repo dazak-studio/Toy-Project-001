b8893c287776857db7947486435db3745845c663
casicos <casicos94@gmail.com>
2018-06-03 22:26:24 +0900

    Add the cube controller (abstract) without the actual controller

---
+++ Assets/Scripts/CubeController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public abstract class CubeController : MonoBehaviour
+{
+	/* properties */
+	public bool IsShootable { get { return ShootCooldownLeft <= Mathf.Epsilon; } }		
+	
+	/* public fields */
+	public float ShootCooldown;
+	
+	/* protected fields */
+	[SerializeField] protected GameObject BulletPrefab;
+	protected float ShootCooldownLeft;
+
+	/* unity callbacks */
+	#region Unity-Callbacks
+	protected void FixedUpdate()
+	{
+		if (!IsShootable)
+			ShootCooldownLeft -= Time.fixedDeltaTime;
+	}
+	#endregion
+
+	/* public methods */
+	public abstract void Shoot();
+}
