b74cd18eb74821724cf2938db01a3b0ef15b8538
jaehosung <jaehossung@gmail.com>
2018-06-03 21:41:23 +0900

    ENH: Player moving and turning

--- game/Assets/PlayerMovement.cs
+++ game/Assets/PlayerMovement.cs
@@ -1,16 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovement : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
+
+	public float speed = 6f;
+
+	private Vector3 movement;
+	private Rigidbody playerRigidbody;
+	private int floorMask;
+	private float camRayLength = 1000f;
+
+	void Awake()
+	{
+		floorMask = LayerMask.GetMask("Floor");
+		playerRigidbody = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void FixedUpdate ()
+	{
+		float h = Input.GetAxisRaw("Horizontal");
+		float v = Input.GetAxisRaw("Vertical");
+		Move(h,v);
+		Turning();
+	}
+
+	void Move(float h, float v)
+	{
+		movement.Set(h, 0f, v);
+		movement = movement.normalized * speed * Time.deltaTime;
+		playerRigidbody.MovePosition(transform.position+movement);
+	}
+
+	void Turning()
+	{
+		Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit floorHit;
+
+		if (Physics.Raycast(camRay, out floorHit, camRayLength, floorMask))
+		{
+			Vector3 playerToMouse = floorHit.point - transform.position;
+			playerToMouse.y = 0f;
+
+			Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
+			playerRigidbody.MoveRotation(newRotation);
+		}
 	}
 }
