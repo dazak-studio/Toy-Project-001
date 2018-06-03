7d7dce6a70d92b99e534eee82594978e13a920fc
headkn <byeongwook.seo@gmail.com>
2018-06-03 21:32:39 +0900

    *edit: move PlayerScript location

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerScript : MonoBehaviour {
 
     private float speed = 5.0f;
+    private int hp = 100;
 
     // Use this for initialization
     private void Awake()
