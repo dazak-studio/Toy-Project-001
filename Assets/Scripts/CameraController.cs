ff99a9172d12dc66ae57f6d49abf087d2913c702
casicos <casicos94@gmail.com>
2018-06-03 22:07:48 +0900

    Add the camera controller to trace a certain object, cube which used by player, and player controller to use player cube

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Transform))]
+public class CameraController : MonoBehaviour
+{
+	public Transform FocusObjectTransform;
+	
+	private Transform _transform;
+	private Vector3 _originPoint;
+
+	private void Awake()
+	{
+		_transform = GetComponent<Transform>();
+		_originPoint = _transform.position;
+	}
+
+	private void LateUpdate()
+	{
+		if (FocusObjectTransform != null)
+			_transform.position = FocusObjectTransform.position + _originPoint;
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Transform))]
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour {
+
+	[SerializeField] private float _forwardSpeed;
+	[SerializeField] private float _backwardSpeed;
+	[SerializeField] private float _rotateSpeed;
+	
+	private Transform _transform;
+	
+	private void Start ()
+	{
+		_transform = GetComponent<Transform>();
+	}
+	
+	private void FixedUpdate()
+	{
+		var horizontal = Input.GetAxis("Horizontal");
+		var vertical = Input.GetAxis("Vertical");
+
+		/* forward and backward move */		
+		var velocity = Vector3.forward * vertical;
+		velocity = _transform.TransformDirection(velocity);
+
+		if (vertical > 0.1f)
+		{
+			velocity *= _forwardSpeed;
+		} else if (vertical < 0.1f)
+		{
+			velocity *= _backwardSpeed;
+		}
+
+		_transform.localPosition += velocity * Time.fixedDeltaTime;
+		
+		/* rotate */
+		_transform.Rotate(0, horizontal * _rotateSpeed, 0);
+	}
+}
