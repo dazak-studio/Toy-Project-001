8381117ade89af487fbf57381a0cb5e6e3b84871
oh3vci <oh3vci@gmail.com>
2018-06-03 22:54:37 +0900

    [ADD] player, enemy prefab with healthBar player can move

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour {
+	public float startHealth = 100;
+	private float health;
+	public Image healthBar;
+
+	public float moveSpeed = 6f;
+
+	Vector3 movement;
+	Rigidbody playerRigidbody;
+	int floorMask;
+	float camRayLength = 100f;
+
+	void Awake()
+	{
+		floorMask = LayerMask.GetMask ("Floor");
+		playerRigidbody = GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate()
+	{
+		float h = Input.GetAxisRaw("Horizontal");
+		float v = Input.GetAxisRaw("Vertical");
+
+		Move(h, v);
+	}
+
+	void Move (float h, float v)
+	{
+		movement.Set (h, 0f, v);
+
+		movement = movement.normalized * moveSpeed * Time.deltaTime;
+
+		playerRigidbody.MovePosition(transform.position + movement);
+	}
+
+	void Start () {
+		health = startHealth;
+		moveSpeed = 7f;
+	}
+
+	public void TakeDamage (float amount)
+	{
+		health -= amount;
+
+		healthBar.fillAmount = health / startHealth;
+
+		if (health <= 0) {
+			// TODO: Die();
+		}
+	}
+}
