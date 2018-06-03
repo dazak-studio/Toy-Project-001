5b6bbabaef51b895330fc76b3c7968416c43375b
headkn <byeongwook.seo@gmail.com>
2018-06-03 21:57:08 +0900

    *added: make HPBar of enemy

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -4,13 +4,32 @@ using UnityEngine;
 
 public class EnemyScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    private int hp;
+    private Transform hpBarTransform;
+
+    private void Awake()
+    {
+        hp = 100;
+        hpBarTransform = transform.GetChild(1).transform;
+    }
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
 	}
+
+    public void DecreaseHP()
+    {
+        Debug.Log("Decrease HP");
+        hp -= 50;
+        hpBarTransform.localScale = new Vector3( (float)hp / 100, 0.1f, 0.1f);
+        if(hp <= 0)
+        {
+            // die
+        }
+    }
 }
