1a28ebbad37279d93ca57e203514832bbc238f50
jaehosung <jaehossung@gmail.com>
2018-06-10 20:44:42 +0900

    CLN: Orgainize Folder

--- Assets/Scripts/PlayerShooting.cs
+++ Assets/Scripts/PlayerShooting.cs
@@ -22,11 +22,13 @@ public class PlayerShooting : MonoBehaviour
 			var angleDeg = transform.rotation.eulerAngles.y;
 			var angle = transform.rotation.eulerAngles.y * Mathf.Deg2Rad;
 			var bulletLength = 1.35f;
-			Vector3 bulletInit = new Vector3( (float)Math.Sin(angle)* bulletLength, 0.5f, (float)Math.Cos(angle) * bulletLength);
+			Vector3 bulletInit = new Vector3( (float)Math.Sin(angle)* bulletLength, 0f,(float)Math.Cos(angle) * bulletLength);
 
 			//Euler angle is the normal angle vectors which is used in UI
 			Quaternion roation_bullet = Quaternion.Euler(90, angleDeg, 0);
-		
+
+			Debug.Log(bulletInit);
+
 			//Rotation Error
 			Instantiate(Bullet, transform.position+bulletInit, roation_bullet);
 		}	
