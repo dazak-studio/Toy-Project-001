16b7c05ab84890372a831c55f1632e83a7096f52
jaehosung <jaehossung@gmail.com>
2018-06-10 20:35:30 +0900

    CLN: Change the root directory

---
+++ Assets/SpawnManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour {
+	public GameObject enemy;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		Instantiate(enemy, new Vector3(4f, 0, 4f), new Quaternion(0,10,0,0));
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
